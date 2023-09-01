1581277f775157701a3e91546c3e149dbae3c688
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2023-09-01 15:31:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     API_ID = 18189325
     API_HASH = "7cb3b3ea70ba49f0721a511013eefe53"
-    TOKEN = "6048858444:AAFWEkj9qNZjTjj3z33jSlfMEl60d45fa1o"  
+    TOKEN = "6048858444:AAFCwQtIfyC-5EHgXdDNuFUokCpIFJb1YOI"  
     START_PIC = "https://telegra.ph/file/e4055f1c03998748f2d55.jpg" 
     CHAT = "-1001840864904"    
